ff4a35933dfbd5bdb325a9c032edd6d96ea33839
NicholasASC66 <crawfordn20@kent-schol.edu>
2019-07-12 13:23:30 -0400

    whiteboards

---
+++ 1(Whiteboard.js
@@ -0,0 +1,11 @@
+const rawtext = "Judge connected on the forst pitch from Ryne Stanek (0-2) to give him nine career multihomer games. ";
+function singleArray = (string){
+    const result = []
+    let stringLower= string.tolowercase()
+    for(let i=0; i<string.length;i++){
+        const char = string[i]
+        if(char is a Number){replace by " "}
+        else if(char is not alpha)
+
+    }return result
+}
