e6c401a05047f8148fe021f3864b94222d4034ef
NicholasASC66 <crawfordn20@kent-schol.edu>
2019-07-15 12:13:48 -0400

    Mario_Paint_draft_1

---
+++ week1/day0/mvp-mario_Paint/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(500,500);
+    
+}
+
+
+//function mouseDragged(){
+    //circle(100,100,100) 
+    //Random()
+
+  //  }
+
+
+function mouseClicked(){
+    circle(mouseX,mouseY 5, 5)
+    circle(mouseX,mouseY 5, 5)
+    return false;
+}
---
+++ week1/day0/p5_intro.js
@@ -0,0 +1,8 @@
+function setup(){
+    createCanvas(1000,1000);
+    background(255,0,0);
+}
+function draw(){
+    circle (500,500,500)
+    triangle (200,200,200)
+}
